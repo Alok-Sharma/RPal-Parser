6b41d2b734ee116910561c1828e760de0d5abeec
Alok Sharma <alok.sharma127@gmail.com>
2016-04-26 00:37:40 -0400

    done creating all stacks and queues for beginning the cse machine.

--- cse_machine.cpp
+++ cse_machine.cpp
@@ -2,18 +2,42 @@
 #include "cse_machine.h"
 #include <iostream>
 #include <queue>
+#include <stack>
+#include <map>
+#include <string>
 
 using namespace std;
 
+//TODO delete everything created using new
+
 void evaluate(Node* root) {
-	generateControlStructures(root);
+	int numOfLambda = countLambda(root);
+	map<string, string> *environs[numOfLambda + 1]; //pointer to an array of maps. Each map is an environment. e0 is at index 0, and so on.
+	queue<cseNode*> *deltas[numOfLambda + 1]; // pointer to an array of queues of cseNodes. Each queue is a delta.
+	generateControlStructures(root, deltas); // populate the array of queues
+
+	stack<cseNode*> programStack; // The program stack	
+	stack<cseNode*> controlStack; // The control stack
+
+	//initilize the control stack;
+	controlStack.push(createNextEnvironment(environs)); //push e0;
+	loadDelta(controlStack, 0, deltas); //push d0;
+
+	startCseMachine(controlStack, programStack, environs, deltas);
+
+	printControlStack(controlStack);
 }
 
-void generateControlStructures(Node* node) {
+void startCseMachine(stack<cseNode*> &controlStack, stack<cseNode*> &programStack, map<string,string>* environs[], queue<cseNode*>* deltas[]) {
+	while(!controlStack.empty()) {
+
+	}
+}
+
+void generateControlStructures(Node* node, queue<cseNode*>* deltas[]) {
 	int numOfLambda = countLambda(node);
 	Node *roots[numOfLambda + 1];
 	roots[0] = node;
-	queue<cseNode*> *deltas[numOfLambda + 1];
 	int count = 0;
 	
 	for(int i = 0; i < numOfLambda + 1; i++) {
@@ -21,21 +45,22 @@ void generateControlStructures(Node* node) {
 		controlStructureHelper(roots[i], *deltas[i], count, roots);	
 	}
 
-	for(int j = 0; j < numOfLambda + 1; j++) {
-		queue<cseNode*> q = *deltas[j];
-		cout << "D" << j << ":\n";
-		while(!q.empty()) {
-			cout << q.front()->name << "\n";
-			q.pop();
-		}
-		cout << "\n";
-	}
+	// for(int j = 0; j < numOfLambda + 1; j++) {
+	// 	queue<cseNode*> q = *deltas[j];
+	// 	cout << "D" << j << ":\n";
+	// 	while(!q.empty()) {
+	// 		cout << q.front()->name << "\n";
+	// 		q.pop();
+	// 	}
+	// 	cout << "\n";
+	// }
 }
 
 void controlStructureHelper (Node* node, queue<cseNode*> &q, int &count, Node* roots[]) {
 	cseNode* csenode = new cseNode;
 
 	if(node->name == "lambda") {
+		csenode->type = "lambda";
 		csenode->name = "lambda";
 		csenode->i = ++count;
 		csenode->x = node->child[0]->name;
@@ -71,5 +96,41 @@ int countLambda(Node* node) {
 		q.pop();
 	}
 	return count;
+}
+
+cseNode* createCseNode(string type, string name) {
+	cseNode* node = new cseNode;
+	node->type = type;
+	node->name = name;
+	return node;
+}
+
+cseNode* createNextEnvironment(map<string, string> *environs[]) {
+	cseNode* envNode = createCseNode("e", "e" + patch::to_string(environCount));
+	environs[environCount] = new map<string,string>();
+	environCount = environCount + 1;
+	return envNode;
+}
+
+void loadDelta(stack<cseNode*> &controlStack, int deltaNumber, queue<cseNode*>* deltas[]) {
+	queue<cseNode*> deltaQueue = *deltas[deltaNumber];
+	// cout << "deltas:\n";
+	while(!deltaQueue.empty()) {
+		// cout << deltaQueue.front()->name << "\n";
+		controlStack.push(deltaQueue.front());
+		deltaQueue.pop();
+	}
+}
+
+//printing a copy of the control stack, not the actual one. So dont worry if stuff is popped.
+void printControlStack(stack<cseNode*> controlStack) {
+	cseNode* node;
+	cout << "\ncontrol stack:" << "\n"; 
+	while(!controlStack.empty()) {
+		node = controlStack.top();
+		cout << node->name << " ";
+		controlStack.pop();
+	}
+	cout << "\n";
 }
 
--- cse_machine.h
+++ cse_machine.h
@@ -5,20 +5,44 @@
 #include "lexer.h"
 #include <queue>
 #include <iostream>
+#include <map>
+#include <stack>
 
 using namespace std;
 
 typedef struct cseNodeType{
+	string type;
 	string name;
 	int i;
 	int k;
 	string x;
 } cseNode;
 
+#include <string>
+#include <sstream>
+
+namespace patch
+{
+    template < typename T > std::string to_string( const T& n )
+    {
+        std::ostringstream stm ;
+        stm << n ;
+        return stm.str() ;
+    }
+}
+
+
+static int environCount = 0;
+
 void evaluate(Node*);
-void generateControlStructures(Node*);
-void controlStructureHelper (Node*, queue<cseNode*>&, int&, Node* roots[]);
+void startCseMachine(stack<cseNode*>&, stack<cseNode*>&, map<string,string>*[], queue<cseNode*>*[]);
+void generateControlStructures(Node*, queue<cseNode*>*[]);
+void controlStructureHelper (Node*, queue<cseNode*>&, int&, Node*[]);
 void preorder(queue<Node*>&, Node*);
-int countLambda(Node*); 
+int countLambda(Node*);
+cseNode* createCseNode(string, string);
+cseNode* createNextEnvironment(map<string, string>*[]);
+void loadDelta(stack<cseNode*>&, int, queue<cseNode*>*[]);
+void printControlStack(stack<cseNode*>);
 
 #endif
